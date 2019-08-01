50467599135b6e3d1a4a71b9ad21ecc3abc2ebeb
Austin Calton <austincalton@gmail.com>
2019-08-01 11:59:20 -0400

    Updates

---
+++ precache-manifest.c4a311b2d637ad01bf4ae6c2151fdc81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1921197781e291ad5808a922381affb5",
+    "url": "/project-management-app/index.html"
+  },
+  {
+    "revision": "4440b5e4d57ef6afa9cd",
+    "url": "/project-management-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1b9fcffcb47a248a032e",
+    "url": "/project-management-app/static/js/2.0912aeeb.chunk.js"
+  },
+  {
+    "revision": "4440b5e4d57ef6afa9cd",
+    "url": "/project-management-app/static/js/main.9c83b20e.chunk.js"
+  },
+  {
+    "revision": "097a8bfd1a597b93a542",
+    "url": "/project-management-app/static/js/runtime~main.c1c7ac6d.js"
+  }
+]);
